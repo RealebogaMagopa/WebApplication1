3384da348ad46032797d4fef306a82c218db579c
RealebogaMagopa <61046703+RealebogaMagopa@users.noreply.github.com>
2020-11-08 19:28:50 +0200

    Add project files.

---
+++ WebApplication1/Controllers/Roles.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class Roles : Controller
+    {
+        RoleManager<IdentityRole> _user;
+
+        public Roles(RoleManager<IdentityRole> user )
+        {
+            _user = user;
+        }
+        public IActionResult Index()
+        {
+            var roles = _user.Roles.ToList();
+            return View(roles);
+        }
+        public IActionResult Create()
+        {
+            return View(new IdentityRole());
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(IdentityRole roles)
+        {
+            await _user.CreateAsync(roles);
+            return RedirectToAction("Index");
+        }
+    }
+}
